47e7b919b27e41e4b06fb2b9dcfbacd3b8c4a6a6
Mário Monteiro <marioammonteiro@gmail.com>
2023-03-03 01:41:13 +0000

    setup boilerplate project done

--- backup.sh
+++ backup.sh
@@ -9,6 +9,7 @@ touch .bak/${FILE_EXCLUDE}
 touch dist/${FILE_EXCLUDE}
 touch node_modules/${FILE_EXCLUDE}
 touch src-tauri/target/${FILE_EXCLUDE}
+touch server/node_modules/${FILE_EXCLUDE}
 
 tar -zcvf ${FILE} \
 	--exclude-tag-all=${FILE_EXCLUDE} \
